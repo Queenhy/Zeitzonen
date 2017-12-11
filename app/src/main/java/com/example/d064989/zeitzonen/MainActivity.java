e087d6d9b86cb39b7769a456e02d5dbc6fb2119e
QTN92 <Quynhi619@gmail.com>
2017-12-11 18:15:24 +0100

    Final Changes

--- app/src/main/java/com/example/d064989/zeitzonen/MainActivity.java
+++ app/src/main/java/com/example/d064989/zeitzonen/MainActivity.java
@@ -9,13 +9,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView time;
+    TextView text;
     String currentTime;
+
+    Intent intentBack;
+
     SharedPreferences prefs;
+    SharedPreferences.Editor e;
+
+    HashMap originalTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,42 +31,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         time = findViewById(R.id.time);
+        text = findViewById(R.id.text);
+
+        originalTimes = new HashMap<String, Integer>();
+        originalTimes.put("tokyo", 8);
+        originalTimes.put("hanoi", 6);
+        originalTimes.put("nyc", -6);
+        originalTimes.put("paris", 0);
+        originalTimes.put("london", -1);
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
-
-        SharedPreferences.Editor e = prefs.edit();
-        e.putString("time", time.getText().toString());
-        e.commit();
-    }
-
-    @Override
-    protected  void onResume(){
+    protected void onResume() {
         super.onResume();
 
-        prefs = getPreferences(MODE_PRIVATE);
-        time.setText(prefs.getString("time", ""));
+        intentBack = getIntent();
+
+        if (getIntent().getExtras() != null) {
+            intentBack.getIntExtra("tokyo", 0);
+            intentBack.getIntExtra("hanoi", 0);
+            intentBack.getIntExtra("nyc", 0);
+            intentBack.getIntExtra("paris", 0);
+            intentBack.getIntExtra("london", 0);
+        }
     }
 
     public void onClick(View aView) {
-        switch (aView.getId())
-        {
+        switch (aView.getId()) {
             case R.id.c1:
-                setCurrentTime("GMT+9", "Tokyo");
+                setCurrentTime("GMT+9", "tokyo");
                 break;
             case R.id.c2:
-                setCurrentTime("GMT+7", "Hanoi");
+                setCurrentTime("GMT+7", "hanoi");
                 break;
             case R.id.c3:
-                setCurrentTime("EST", "New York");
+                setCurrentTime("EST", "nyc");
                 break;
             case R.id.c4:
-                setCurrentTime("CET", "Paris");
+                setCurrentTime("CET", "paris");
                 break;
             case R.id.c5:
-                setCurrentTime("GMT", "London");
+                setCurrentTime("GMT", "london");
                 break;
             case R.id.w:
                 openWartung();
@@ -68,13 +81,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void openWartung() {
         Intent intent = new Intent(this, timeSet.class);
+
+        if (getIntent().getExtras() != null) {
+            intent.putExtra("tokyo", intentBack.getIntExtra("tokyo", 0));
+            intent.putExtra("hanoi", intentBack.getIntExtra("hanoi", 0));
+            intent.putExtra("nyc", intentBack.getIntExtra("nyc", 0));
+            intent.putExtra("paris", intentBack.getIntExtra("paris", 0));
+            intent.putExtra("london", intentBack.getIntExtra("london", 0));
+        } else {
+            intent.putExtra("tokyo", (int) originalTimes.get("tokyo"));
+            intent.putExtra("hanoi", (int) originalTimes.get("hanoi"));
+            intent.putExtra("nyc", (int) originalTimes.get("nyc"));
+            intent.putExtra("paris", (int) originalTimes.get("paris"));
+            intent.putExtra("london", (int) originalTimes.get("london"));
+        }
         currentTime = time.getText().toString();
 
         if (!currentTime.isEmpty()) {
-            intent.putExtra("time", currentTime);
             startActivity(intent);
-        }
-        else Toast.makeText(getApplicationContext(), "Bitte wählen Sie eine Zeitzone aus!",
+        } else Toast.makeText(getApplicationContext(), "Bitte wählen Sie eine Zeitzone aus!",
                 Toast.LENGTH_SHORT).show();
 
     }
@@ -84,7 +109,20 @@ public class MainActivity extends AppCompatActivity {
         TimeZone tz = java.util.TimeZone.getTimeZone(timezone);
         Calendar c = java.util.Calendar.getInstance(tz);
 
-        currentTime = c.get(java.util.Calendar.HOUR_OF_DAY)+":"+c.get(java.util.Calendar.MINUTE)+":"+c.get(java.util.Calendar.SECOND);
-        time.setText("Zeit in " + city + ": " + currentTime);
+        currentTime = c.get(java.util.Calendar.HOUR_OF_DAY) + ":" + c.get(java.util.Calendar.MINUTE) + ":" + c.get(java.util.Calendar.SECOND);
+
+        int originalTimeshift = (int) originalTimes.get(city);
+        int plusHours = getIntent().getIntExtra(city, 0);
+
+        if (plusHours != originalTimeshift) {
+            plusHours = plusHours - originalTimeshift;
+            c.add(Calendar.HOUR_OF_DAY, +plusHours);
+            currentTime = c.get(java.util.Calendar.HOUR_OF_DAY) + ":" + c.get(java.util.Calendar.MINUTE) + ":" + c.get(java.util.Calendar.SECOND);
+        }
+
+        text.setText("Zeit in " + city + ":");
+        time.setText(currentTime);
     }
+
+
 }
--- app/src/main/java/com/example/d064989/zeitzonen/timeSet.java
+++ app/src/main/java/com/example/d064989/zeitzonen/timeSet.java
@@ -1,23 +1,25 @@
 package com.example.d064989.zeitzonen;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Objects;
 
 public class timeSet extends AppCompatActivity {
 
-    String currentTime;
     Intent intent;
-    TextView time;
-    String newTime;
+    TextView text;
+    int zeitverschiebung = 0;
+
+    RadioGroup radioGroup;
+    SharedPreferences prefsMain;
+    SharedPreferences.Editor e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +27,141 @@ public class timeSet extends AppCompatActivity {
         setContentView(R.layout.activity_time_set);
 
         intent = getIntent();
-        currentTime = intent.getStringExtra("time");
-        time = findViewById(R.id.time);
-        time.setText(currentTime);
+        text = findViewById(R.id.text);
+
+        prefsMain = getPreferences(MODE_PRIVATE);
+        e = prefsMain.edit();
+        e.putInt("tokyo", intent.getIntExtra("tokyo",0));
+        e.putInt("hanoi", intent.getIntExtra("hanoi",0));
+        e.putInt("nyc", intent.getIntExtra("nyc",0));
+        e.putInt("paris", intent.getIntExtra("paris",0));
+        e.putInt("london", intent.getIntExtra("london",0));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        e.putInt("tokyo", prefsMain.getInt("tokyo", 0));
+        e.putInt("hanoi", prefsMain.getInt("hanoi", 0));
+        e.putInt("nyc", prefsMain.getInt("nyc", 0));
+        e.putInt("paris", prefsMain.getInt("paris", 0));
+        e.putInt("london", prefsMain.getInt("london", 0));
+        e.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        prefsMain.getInt("tokyo", 0);
+        prefsMain.getInt("hanoi", 0);
+        prefsMain.getInt("nyc", 0);
+        prefsMain.getInt("paris", 0);
+        prefsMain.getInt("london", 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        e.putInt("tokyo", prefsMain.getInt("tokyo", 0));
+        e.putInt("hanoi", prefsMain.getInt("hanoi", 0));
+        e.putInt("nyc", prefsMain.getInt("nyc", 0));
+        e.putInt("paris", prefsMain.getInt("paris", 0));
+        e.putInt("london", prefsMain.getInt("london", 0));
+        e.commit();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("tokyo", prefsMain.getInt("tokyo", 0));
+        intent.putExtra("hanoi", prefsMain.getInt("hanoi", 0));
+        intent.putExtra("nyc", prefsMain.getInt("nyc", 0));
+        intent.putExtra("paris", prefsMain.getInt("paris", 0));
+        intent.putExtra("london", prefsMain.getInt("london", 0));
+        startActivity(intent);
     }
 
     public void onClick(View button) throws ParseException {
-        switch (button.getId()){
-            case R.id.minus:
-                calculation("minus");
+        int selectedId = checkRadioButton();
+
+        if (!(selectedId == 0)) {
+            switch (button.getId()) {
+                case R.id.minus:
+                    preCalculation("minus", selectedId);
+                    break;
+                case R.id.plus:
+                    preCalculation("plus", selectedId);
+                    break;
+            }
+        }
+    }
+
+    private void preCalculation(String calc, int selectedId) throws ParseException {
+        switch (selectedId) {
+            case R.id.tokyo:
+                zeitverschiebung = prefsMain.getInt("tokyo", 0);
+                calculation(calc, "tokyo");
                 break;
-            case R.id.plus:
-                calculation("plus");
+            case R.id.hanoi:
+                zeitverschiebung = prefsMain.getInt("hanoi", 0);
+                calculation(calc, "hanoi");
+                break;
+            case R.id.newyork:
+                zeitverschiebung = prefsMain.getInt("nyc", 0);
+                calculation(calc, "nyc");
+                break;
+            case R.id.paris:
+                zeitverschiebung = prefsMain.getInt("paris", 0);
+                calculation(calc, "paris");
+                break;
+            case R.id.london:
+                zeitverschiebung = prefsMain.getInt("london", 0);
+                calculation(calc, "london");
                 break;
         }
     }
 
-    public void calculation(String calc) throws ParseException {
-        currentTime = time.getText().toString();
-        SimpleDateFormat sdfkk = new SimpleDateFormat("HH:mm:ss");
-        Date cTime = sdfkk.parse(currentTime);
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(cTime);
+    public void calculation(String calc, String city) throws ParseException {
 
-        if (Objects.equals(calc, "minus")) {
-            instance.add(Calendar.MINUTE, -60);
+        if (calc == "minus") {
+            zeitverschiebung -= 1;
+        } else {
+            zeitverschiebung += 1;
         }
-        else instance.add(Calendar.MINUTE, 60);
+        text.setText("Die Zeitverschiebung zu Berlin beträgt: " + zeitverschiebung);
+        e.putInt(city, zeitverschiebung);
+        e.commit();
+    }
 
-        newTime = instance.get(java.util.Calendar.HOUR_OF_DAY)+":"+instance.get(java.util.Calendar.MINUTE)+":"+instance.get(java.util.Calendar.SECOND);
-        time.setText(newTime);
+    public void onRadioButtonClicked(View rb) {
+        int rbId = rb.getId();
+        switch (rbId) {
+            case R.id.tokyo:
+                text.setText("Die Zeitverschiebung zu Berlin beträgt " + prefsMain.getInt("tokyo", 0));
+                break;
+            case R.id.hanoi:
+                text.setText("Die Zeitverschiebung zu Berlin beträgt " + prefsMain.getInt("hanoi", 0));
+                break;
+            case R.id.newyork:
+                text.setText("Die Zeitverschiebung zu Berlin beträgt " + prefsMain.getInt("nyc", 0));
+                break;
+            case R.id.paris:
+                text.setText("Die Zeitverschiebung zu Berlin beträgt " + prefsMain.getInt("paris", 0));
+                break;
+            case R.id.london:
+                text.setText("Die Zeitverschiebung zu Berlin beträgt " + prefsMain.getInt("london", 0));
+                break;
+        }
+
+    }
+
+    private int checkRadioButton() {
+        int selectedId = 0;
+        radioGroup = findViewById(R.id.radiogroup);
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(getApplicationContext(), "Please select a timezone!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            selectedId = radioGroup.getCheckedRadioButtonId();
+        }
+        return selectedId;
     }
 }
